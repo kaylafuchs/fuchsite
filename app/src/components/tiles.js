e970df0256d423cf00f0ec3526e625066bda6f8e
kaylafuchs <kfuchs24@gmail.com>
2019-01-20 12:42:40 -0500

    css grid feature is NUTS

--- app/src/components/tiles.js
+++ app/src/components/tiles.js
@@ -4,76 +4,14 @@ import '../styles/tiles.scss';
 
 const Tiles = () => {
     return (
-        <div className="tile-container">
-            <div className="tile-col">
-                <div className="tile-row">
-                    <div className="tile tile-invisible"></div>
-                    <div className="tile tile-xl a">Two</div>
-                </div>
-                <div className="tile-row baseline">
-                    <div className="tile-col inner">
-                        <div className="tile tile-invisible"></div>  
-                    </div>
-                    <div className="tile-col inner wide">
-                        <div className="tile-row right">
-                            <div className="tile b">Three</div>
-                        </div>
-                    </div>
-                    <div className="tile-col inner">
-                        <div className="tile-row around baseline">
-                            <div className="tile c">Three.1</div>
-                            <div className="tile c">Three.2</div>
-                        </div>
-                        <div className="tile-row">
-                            <div className="tile g">Three.3</div>
-                        </div>
-                    </div>
-                </div>
-                {/* <div className="tile tile-m tile-invisible">
-                    <div className="tile-col inner">
-                        <div className="tile tile-s tile-invisible">Three.1</div>
-                        <div className="tile tile-s tile-invisible">Three.2</div>
-                    </div>
-                    <div className="tile-col inner">
-                        <div className="tile tile-s b">Three.3</div>
-                        <div className="tile tile-s c">Three.4</div>
-                    </div>
-                </div> */}
-                <div className="tile-row">
-                    <div className="tile-col inner">
-                        <div className="tile tile-invisible"></div>  
-                    </div>
-                    <div className="tile-col inner">
-                        <div className="tile-row right">
-                            <div className="tile tile-invisible"></div> 
-                            <div className="tile d">Four</div>
-                        </div>
-                    </div>
-                </div>          
-            </div>
-            <div className="tile-col">
-                {/* <div className="tile tile-s tile-invisible">One</div> */}
-                {/* <div className="tile">Five</div> */}
-                <div className="tile tile-l d">Six</div>
-                <div className="tile tile-s ">Seven</div>
-                <div className="tile">Eight</div>
-            </div>
-            <div className="tile-col">
-                <div className="tile tile-invisible">
-                    <div className="tile-col inner">
-                        <div className="tile tile-s tile-invisible"></div>
-                        <div className="tile tile-m">Nine.2</div>
-                    </div>
-                    <div className="tile-col inner">
-                    <div className="tile tile-s tile-invisible"></div>
-                    </div>
-                </div>
-                <div className="tile tile-xl">Ten</div>
-                <div className="tile tile-invisible">Eleven</div>
-                {/* <div className="tile">Twelve</div>
-                <div className="tile">Thirteen</div>
-                <div className="tile">Fourteen</div> */}
-            </div>
+        <div className="grid">
+            <div className="card row-2-3 col-3-3 c-1">Three</div>
+            <div className="card row-4-3 col-6-3 c-1">Four</div>
+            <div className="card row-5-2 col-2-2 c-1">Five</div>
+            <div className="card row-5-1 col-4-1 c-1">Six</div>
+            <div className="card row-5-1 col-5-1 c-1">Seven</div>
+            <div className="card row-6-1 col-4-2 c-1">Eight</div>
+            <div className="card row-6-1 col-4-2 c-1">Nine</div>
         </div>
     )
 }
