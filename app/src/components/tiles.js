49716dc86c753779472fc0ea078cb30397a20b29
kaylafuchs <kfuchs24@gmail.com>
2019-01-20 13:30:06 -0500

    grid layout

--- app/src/components/tiles.js
+++ app/src/components/tiles.js
@@ -4,15 +4,27 @@ import '../styles/tiles.scss';
 
 const Tiles = () => {
     return (
-        <div className="grid">
-            <div className="card row-2-3 col-3-3 c-1">Three</div>
-            <div className="card row-4-3 col-6-3 c-1">Four</div>
-            <div className="card row-5-2 col-2-2 c-1">Five</div>
-            <div className="card row-5-1 col-4-1 c-1">Six</div>
-            <div className="card row-5-1 col-5-1 c-1">Seven</div>
-            <div className="card row-6-1 col-4-2 c-1">Eight</div>
-            <div className="card row-6-1 col-4-2 c-1">Nine</div>
-        </div>
+        // <div className="container">
+            <div className="grid">
+                <div className="card row-1-3 col-3-3 c-1">A</div>
+                <div className="card row-3-3 col-6-2 c-1">B</div>
+                <div className="card row-4-2 col-2-2 c-1">C</div>
+                <div className="card row-4-1 col-4-1 c-1">D</div>
+                <div className="card row-4-1 col-5-1 c-1">E</div>
+                <div className="card row-5-1 col-4-2 c-1">F</div>
+                <div className="card row-5-1 col-4-2 c-1">G</div>
+                <div className="card row-5-1 col-4-2 c-1">H</div>
+                <div className="card row-6-3 col-3-2 c-1">I</div>
+                <div className="card row-6-2 col-5-2 c-1 round">J</div>
+                <div className="card row-6-3 col-7-3 c-1">K</div>
+                <div className="card row-2-2 col-8-1 c-1">L</div>
+                <div className="card row-3-2 col-9-2 c-1">M</div>
+                <div className="card row-2-1 col-9-1 c-1 round">N</div>
+                <div className="card row-4-1 col-8-1 c-1">O</div>
+                <div className="card row-5-1 col-8-2 c-1">P</div>
+                <div className="card row-6-1 col-10-1 c-1">Q</div>
+            </div>
+        // </div>
     )
 }
 export default Tiles
