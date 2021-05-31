e57cb8b5d929fa8115f3625d37dbd60ac201c875
kaylafuchs <kaylafuchs@protonmail.com>
2021-05-30 22:48:17 -0500

    update location

--- app/src/components/home.js
+++ app/src/components/home.js
@@ -32,7 +32,7 @@ class Home extends React.Component {
                         </div>
                     </div>
                     <div className="mobile-row">
-                        <div className="tile location-mobile f">Brooklyn, NY</div>
+                        <div className="tile location-mobile f">Nashville, TN</div>
                     </div>
                     <div className="mobile-row">
                         <Link to="/resume" className="tile link-mobile b">Resume</Link>
@@ -53,7 +53,7 @@ class Home extends React.Component {
                     <Link to="/resume" className="tile b">Resume</Link>
                     <div className="tile d"></div>
                     <div className="tile e"></div>
-                    <div className="tile f">Brooklyn, NY</div>
+                    <div className="tile f">Nashville, TN</div>
                     <Link to="/creative-work" className="tile g">Creative Work</Link>
                     <a className="tile h round" href="mailto:kfuchs24@gmail.com">Contact
                         <svg xmlns="http://www.w3.org/2000/ svg" width="40" height="40" viewBox="0 0 24 24"><path fill="#2E06AE" d="M0 3v18h24v-18h-24zm6.623 7.929l-4.623 5.712v-9.458l4.623 3.746zm-4.141-5.929h19.035l-9.517 7.713-9.518-7.713zm5.694 7.188l3.824 3.099 3.83-3.104 5.612 6.817h-18.779l5.513-6.812zm9.208-1.264l4.616-3.741v9.348l-4.616-5.607z"/></svg>
